55a9ca70a37158e791ab525214ee61d114cce175
Aurgha Das <145524283+aurghadas@users.noreply.github.com>
2024-12-28 22:52:01 -0500

    Update Stock.java 2.0

--- ePortfolio/Stock.java
+++ ePortfolio/Stock.java
@@ -53,3 +53,49 @@ public class Stock extends Investment{
         return true;
     }
 
+
+/**
+     * Buys additional quantities of the stock at a specified price.
+     * If the user chooses to buy the first stock, the book value is initialized
+     * based on the quantity and price provided, including the commission fee.
+     * If the user chooses to buy more shares of the same stock at the same price,
+     * the book value is updated by adding the new purchase cost to the existing
+     * book value. If the user chooses to buy more shares at a different price,
+     * the book value is updated by adding the new purchase cost to the existing
+     * book value and the price is updated to the new price.
+     * 
+     * @param quantity the number of shares to buy
+     * @param price the price per share to use for the purchase
+     * @return a string indicating the success of the purchase and the updated details of the stock
+     */
+    @Override
+    public String buy(int quantity, double price){
+        this.updatePrice(price); //Updating the price first
+        this.updateQuantity(quantity);  //Updating the quantity
+
+        if(this.quantity == 0){  //Case when buying the first stock
+            this.bookValue = (quantity * price) + comission;
+        }else{
+            if(this.price == price){  //Case when updated price is same as before
+                this.bookValue += (quantity * price + comission);
+                return "Same price! Adding to the book value for the following stock:" + "\n\n" +
+                        "Symbol: " + this.getSymbol() + "\n" +
+                        "Name: " + this.getName() + "\n" +
+                        "Quantity: " + this.getQuantity() + "\n" +
+                        "Price: " + this.getPrice() + "\n" +
+                        "Book Value: " + this.getBookValue();
+            }else{  //Case when updated price is new
+                this.bookValue = this.bookValue + (quantity * price + comission);
+                this.price = price;
+                return "New price! Updating the price and adjusting book value for the following stock:" + "\n\n" +
+                        "Symbol: " + this.getSymbol() + "\n" +
+                        "Name: " + this.getName() + "\n" +
+                        "Quantity: " + this.getQuantity() + "\n" +
+                        "Price: " + this.getPrice() + "\n" +
+                        "Book Value: " + this.getBookValue();
+            }
+        }
+        this.quantity += quantity;  //Updating the quantity according to the purchase amount
+        return "Purchased successfully!";
+    }
+
